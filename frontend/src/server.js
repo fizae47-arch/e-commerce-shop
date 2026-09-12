2e4aef0f72be2e6b18d50afc132c71dbc5a72aed
Fiza Eman <fizae47@gmail.com>
2026-09-12 21:17:33 +0500

    Configure frontend backend URL

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,7 @@
-export const backend_url = "http://localhost:3000";
+// export const backend_url = "http://localhost:3000";
+// export const server = `${backend_url}/api/v2`;
+
+
+
+export const backend_url = "https://e-commerce-shop-gamma-nine.vercel.app";
 export const server = `${backend_url}/api/v2`;
